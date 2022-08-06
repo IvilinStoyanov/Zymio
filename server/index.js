1871a4324e7e6abe1260572c2f755e868e31a707
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-08-06 20:46:39 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'Zymio app 3' });
+    res.send({ hi: 'Zymio app - test auto deploy functionality' });
 });
 
 
