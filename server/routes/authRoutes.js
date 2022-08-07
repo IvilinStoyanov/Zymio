df5d00ac5d1a3eb109fd59fe5117dbb47753c9db
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-08-07 18:25:58 +0300

    refactoring

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+
+module.exports = app => {
+    app.get('/', (req, res) => {
+        res.send({ name: 'Zymio' });
+    });
+    
+    app.get(
+        '/auth/google',
+        passport.authenticate('google', {
+            scope: ['profile', 'email']
+        })
+    );
+    
+    app.get(
+        '/auth/google/callback',
+        passport.authenticate('google'),
+    );
+};
