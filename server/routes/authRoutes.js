b7c3cfd49a4b978467eb02ddcdbfb8e9487d7581
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-08-08 18:29:45 +0300

    extend the OAuth flow

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,16 +4,25 @@ module.exports = app => {
     app.get('/', (req, res) => {
         res.send({ name: 'Zymio' });
     });
-    
+
     app.get(
         '/auth/google',
         passport.authenticate('google', {
             scope: ['profile', 'email']
         })
     );
-    
+
     app.get(
         '/auth/google/callback',
         passport.authenticate('google'),
     );
+
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user);
+    });
+
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    });
 };
