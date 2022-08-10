d6354ff5632bdfaeb0d80879b57209c7aa171a9f
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-08-10 12:45:38 +0300

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,6 +3,7 @@ const keys = require('../config/keys');
 
 module.exports = app => {
     app.get('/', (req, res) => {
+        
         res.send({ name: 'Zymio test', URI: keys.mongoURI });
     });
 
