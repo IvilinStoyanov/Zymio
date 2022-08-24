3cb967271039022deb10205229c68a4790b12bda
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-08-25 01:38:08 +0300

    finishing reducer setup

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { applyMiddleware, combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 
 import App from './components/App';
+import reducers from './reducers';
+
+const reducer = combineReducers({ reducers });
+
+const store = configureStore({ reducer }, applyMiddleware());
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}><App /></Provider>
     </React.StrictMode>
 );
