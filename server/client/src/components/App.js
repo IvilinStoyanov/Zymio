75be834b00db9dbe2bd49a4d8380a7d925ee6c48
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-08-26 17:47:48 +0300

    route configuration

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const ServeyNew = () => <h2>HeaderServeyNew</h2>;
+const Landing = () => <h2>HeaderLanding</h2>;
 
 const App = () => {
     return (
         <div>
             Zymio
+            <BrowserRouter>
+                <div>
+                    <Route path="/" component={Landing} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
