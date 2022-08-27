a424472939e389cae2fc5380f89ea6933dc497b8
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-08-27 15:04:09 +0300

    styling a bit the header component

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>HeaderServeyNew</h2>;
-const Landing = () => <h2>HeaderLanding</h2>;
+const SurveyNew = () => <h2>Header Servey New</h2>;
+const Landing = () => <h2>Header Landing</h2>;
 
 const App = () => {
     return (
--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <div>
+                        <a className="brand-logo">Zymio</a>
+                    </div>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li>
+                            <a>Login with Google</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
