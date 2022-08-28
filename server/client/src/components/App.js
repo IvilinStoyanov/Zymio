6891b7191b75cc14df08d28c15c8ba1b6fc858c1
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-08-28 19:20:05 +0300

    refactor the configureStore function

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>Header Servey New</h2>;
 const Landing = () => <h2>Header Landing</h2>;
 
-const App = () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <Header />
-                <div>
-                    <Route path="/" exact component={Landing} />
-                    <Route path="/surveys" exact component={Dashboard} />
-                    <Route path="/surveys/new" component={SurveyNew} />
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+class App extends Component {
+componentDidMount() {
+    console.log("Did mount called");
+    this.props.fetchUser();
+}
+
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <Header />
+                    <div>
+                        <Route path="/" exact component={Landing} />
+                        <Route path="/surveys" exact component={Dashboard} />
+                        <Route path="/surveys/new" component={SurveyNew} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    };
 };
 
-export default App;
+export default connect(null, actions)(App);
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -2,20 +2,18 @@ import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { applyMiddleware, combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
-import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const reducer = combineReducers({ reducers });
-
-const store = configureStore({ reducer }, applyMiddleware());
+const store =
+    configureStore({
+        reducer: reducers,
+        middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false }),
+    });
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}><App /></Provider>
-    </React.StrictMode>
+    <Provider store={store}><App /></Provider>
 );
