91022766a9db005ad535fca4e8b8abce109b3d55
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-09-07 20:51:18 +0300

    extend survey route logic

--- server/routes/surveyRoute.js
+++ server/routes/surveyRoute.js
@@ -1,7 +1,25 @@
+const mongoose = require('mongoose');
+
 const requireLogin = require('../middlewares/requireLogin');
+const requireCredit = require('../middlewares/requireCredits');
+
+const Survey = mongoose.model('surveys');
 
 module.export = app => {
-    app.post('/api/surveys', requireLogin, (req, res) => {    
+    app.post('/api/surveys',
+        requireLogin,
+        requireCredit,
+        (req, res) => {
+            const { title, subject, body, recipients } = req.body;
 
-    });
+            const survey = new Survey({
+                title,
+                subject,
+                body,
+                recipient: recipients.split(',').map(email => ({ email })),
+                _user: req.user.id,
+                dateSent: new Date(),
+                lastResponded: new Date()
+            });
+        });
 };
