3dfc5c62273a96f22c4edf23afe302547e050ce3
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-09-15 11:14:38 +0300

    fixing a bug with the logic in validate emails function

--- server/client/src/util/validateEmails.js
+++ server/client/src/util/validateEmails.js
@@ -1,12 +1,11 @@
-const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-debugger
+const regex = /^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const validateEmails = emails => {
     const invalidEmails = emails
         .split(',')
         .map(email => email.trim())
         .filter(email => !regex.test(email));
 
-    if (invalidEmails.lenght)
+    if (invalidEmails.length)
         return `These emails are invalid ${invalidEmails}`;
 
 };
